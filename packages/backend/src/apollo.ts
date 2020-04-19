86b3799ddb48baa62430e648b8eaabed7a3186eb
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-04-19 19:34:29 +0200

    Added backend graphql stuffs

---
+++ packages/backend/src/apollo.ts
@@ -0,0 +1,12 @@
+import { ApolloServer } from 'apollo-server';
+import { buildSchema } from 'type-graphql';
+
+async function createApolloServer() {
+	const schema = await buildSchema({
+		resolvers: [__dirname + '/modules/**/*.{mutation,query,field}.{js,ts}'],
+	});
+
+	return new ApolloServer({ schema });
+}
+
+export default createApolloServer;
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,4 +1,20 @@
+import 'reflect-metadata';
+
+import RedisSession from 'connect-redis';
+import dotenv from 'dotenv';
 import express from 'express';
+import session from 'express-session';
+import redis from 'redis';
+
+import createApolloServer from './apollo';
+
+dotenv.config();
+
+const redisClient = redis.createClient({
+	host: 'redis',
+	password: String(process.env.REDIS_PASSWORD),
+});
+const RedisSessionStore = RedisSession(session);
 
 (async function () {
 	try {
@@ -7,6 +23,18 @@ import express from 'express';
 		app.get('/', (req, res) => {
 			return res.send(req.query);
 		});
+
+		app.use(
+			session({
+				store: new RedisSessionStore({ client: redisClient }),
+				secret: String(process.env.SESSION_SECRET),
+				resave: false,
+				saveUninitialized: false,
+			}),
+		);
+
+		const server = await createApolloServer();
+		server.applyMiddleware({ app });
 
 		app.listen(3000, () => {
 			console.log(`Server ready`);
