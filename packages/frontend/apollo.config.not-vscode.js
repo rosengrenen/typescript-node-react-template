a52dec47ee4ac0b7223415e4a5548ce23b660481
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-04-23 22:28:58 +0200

    Fixed schema generation at build time and vs code integration

--- packages/frontend/apollo.config.not-vscode.js
+++ packages/frontend/apollo.config.not-vscode.js
@@ -2,7 +2,7 @@ module.exports = {
 	client: {
 		service: {
 			name: 'template-project',
-			localSchemaFile: './packages/backend/src/__generated__/schema.gql',
+			localSchemaFile: '../backend/src/__generated__/schema.gql',
 			excludes: ['**/__tests__/**/*'],
 		},
 	},
