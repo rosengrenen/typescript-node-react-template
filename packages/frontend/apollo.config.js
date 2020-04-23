338ce95beef923208eab6835e9bf2246211e1da9
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-04-23 13:22:21 +0200

    added apollo utils

---
+++ packages/frontend/apollo.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	client: {
+		service: {
+			name: 'template-project',
+			url: 'http://localhost:8080/graphql',
+			// localSchemaFile: './test.gql',
+			// excludes: ['**/__tests__/**/*'],
+		},
+	},
+};
